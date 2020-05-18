9da2361f9b73824fb56c07e096c6891174b5652b
Anand <abhishektiwary02@gmail.com>
2020-05-18 14:40:10 +0530

    make it better

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyAjfHBn7D7FvlR0a_ykXnHpYrTWvpTpsJw",
+    authDomain: "happy-place-50bae.firebaseapp.com",
+    databaseURL: "https://happy-place-50bae.firebaseio.com",
+    projectId: "happy-place-50bae",
+    storageBucket: "happy-place-50bae.appspot.com",
+    messagingSenderId: "385963166900",
+    appId: "1:385963166900:web:07e93d8efdb742f6719be1",
+    measurementId: "G-6ZWCZF1SFC"
+  }
 };
